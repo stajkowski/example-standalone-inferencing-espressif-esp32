22bcf1479f2e3b3d70c1a5f5709e92c4cfb6af2c
Brian Stajkowski <stajkowski100@gmail.com>
2024-08-10 20:59:37 -0700

    Testing EI w/ TensorFlow

---
+++ main/include/app_camera_esp.h
@@ -0,0 +1,72 @@
+/* Copyright 2019 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#ifndef APP_CAMERA_ESP_H_
+#define APP_CAMERA_ESP_H_
+
+#include "sensor.h"
+#include "esp_camera.h"
+#include "esp_log.h"
+#include "esp_system.h"
+
+// ESP32Cam (AiThinker) PIN Map
+#define CAMERA_MODULE_NAME "ESP-DEVCAM"
+#define CAMERA_PIN_PWDN 32
+#define CAMERA_PIN_RESET -1 //software reset will be performed
+#define CAMERA_PIN_XCLK 0
+#define CAMERA_PIN_SIOD 26
+#define CAMERA_PIN_SIOC 27
+
+#define CAMERA_PIN_D7 35
+#define CAMERA_PIN_D6 34
+#define CAMERA_PIN_D5 39
+#define CAMERA_PIN_D4 36
+#define CAMERA_PIN_D3 21
+#define CAMERA_PIN_D2 19
+#define CAMERA_PIN_D1 18
+#define CAMERA_PIN_D0 5
+#define CAMERA_PIN_VSYNC 25
+#define CAMERA_PIN_HREF 23
+#define CAMERA_PIN_PCLK 22
+
+/**
+ * PIXFORMAT_RGB565,    // 2BPP/RGB565
+ * PIXFORMAT_YUV422,    // 2BPP/YUV422
+ * PIXFORMAT_GRAYSCALE, // 1BPP/GRAYSCALE
+ * PIXFORMAT_JPEG,      // JPEG/COMPRESSED
+ * PIXFORMAT_RGB888,    // 3BPP/RGB888
+ */
+#define CAMERA_PIXEL_FORMAT PIXFORMAT_YUV422
+/*
+ * FRAMESIZE_96X96,    // 96x96
+ * FRAMESIZE_QQVGA,    // 160x120
+ * FRAMESIZE_QQVGA2,   // 128x160
+ * FRAMESIZE_QCIF,     // 176x144
+ * FRAMESIZE_HQVGA,    // 240x176
+ * FRAMESIZE_QVGA,     // 320x240
+ * FRAMESIZE_CIF,      // 400x296
+ * FRAMESIZE_VGA,      // 640x480
+ * FRAMESIZE_SVGA,     // 800x600
+ * FRAMESIZE_XGA,      // 1024x768
+ * FRAMESIZE_SXGA,     // 1280x1024
+ * FRAMESIZE_UXGA,     // 1600x1200
+ */
+#define CAMERA_FRAME_SIZE FRAMESIZE_VGA
+
+#define XCLK_FREQ_HZ 15000000
+
+int app_camera_init();
+
+#endif
--- main/main.cpp
+++ main/main.cpp
@@ -22,31 +22,160 @@
 
 /* Include ----------------------------------------------------------------- */
 #include <stdio.h>
+#include <esp_system.h>
+#include <nvs_flash.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 
 #include "edge-impulse-sdk/classifier/ei_run_classifier.h"
 
+extern "C" {
+    #include "app_camera_esp.h"
+    #include "connect_wifi.h"
+}
 #include "driver/gpio.h"
+#include "esp_camera.h"
+#include "esp_timer.h"
 #include "sdkconfig.h"
+#include "esp_err.h"
 #include "esp_idf_version.h"
+#include "esp_http_server.h"
 
-#define LED_PIN GPIO_NUM_21
+#define EI_CAMERA_RAW_FRAME_BUFFER_COLS           640
+#define EI_CAMERA_RAW_FRAME_BUFFER_ROWS           450
+#define EI_CAMERA_FRAME_BYTE_SIZE                 3
 
-static const float features[] = {
-    // copy raw features here (for example from the 'Live classification' page)
-};
+#define PART_BOUNDARY "123456789000000000000987654321"
+static const char* _STREAM_CONTENT_TYPE = "multipart/x-mixed-replace;boundary=" PART_BOUNDARY;
+static const char* _STREAM_BOUNDARY = "\r\n--" PART_BOUNDARY "\r\n";
+static const char* _STREAM_PART = "Content-Type: image/jpeg\r\nContent-Length: %u\r\n\r\n";
 
-void setup_led() {
-#if ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(5, 0, 0)
-    esp_rom_gpio_pad_select_gpio(LED_PIN);
-#elif ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(4, 0, 0)
-    gpio_pad_select_gpio(LED_PIN);
-#endif
-    gpio_set_direction(LED_PIN, GPIO_MODE_OUTPUT);
+static const char *TAG = "esp32-cam Webserver";
+
+uint8_t *snapshot_buf; //points to the output of the capture
+
+
+// Get an image from the camera module
+esp_err_t get_image(int image_width, int image_height, uint8_t* image_data) {
+  camera_fb_t* fb = esp_camera_fb_get();
+  if (!fb) {
+    ESP_LOGE("camera", "Camera capture failed");
+    return false;
+  }
+
+  ei_printf("Image Captured\n");
+  // We have initialised camera to grayscale
+  // Just quantize to int8_t
+  for (int i = 0; i < image_width * image_height; i++) {
+    image_data[i] = ((uint8_t *) fb->buf)[i] ^ 0x80;
+  }
+
+  esp_camera_fb_return(fb);
+  /* here the esp camera can give you grayscale image directly */
+  return true;
 }
 
-int raw_feature_get_data(size_t offset, size_t length, float *out_ptr) {
-  memcpy(out_ptr, features + offset, length * sizeof(float));
+static int ei_camera_get_data(size_t offset, size_t length, float *out_ptr)
+{
+  // we already have a RGB888 buffer, so recalculate offset into pixel index
+  size_t pixel_ix = offset * 3;
+  size_t pixels_left = length;
+  size_t out_ptr_ix = 0;
+
+  while (pixels_left != 0) {
+    out_ptr[out_ptr_ix] = (snapshot_buf[pixel_ix] << 16) + (snapshot_buf[pixel_ix + 1] << 8) + snapshot_buf[pixel_ix + 2];
+
+    // go to the next pixel
+    out_ptr_ix++;
+    pixel_ix += 3;
+    pixels_left--;
+  }
+  // and done!
   return 0;
+}
+
+esp_err_t jpg_stream_httpd_handler(httpd_req_t *req){
+    camera_fb_t * fb = NULL;
+    esp_err_t res = ESP_OK;
+    size_t _jpg_buf_len;
+    uint8_t * _jpg_buf;
+    char * part_buf[64];
+    static int64_t last_frame = 0;
+    if(!last_frame) {
+        last_frame = esp_timer_get_time();
+    }
+
+    res = httpd_resp_set_type(req, _STREAM_CONTENT_TYPE);
+    if(res != ESP_OK){
+        return res;
+    }
+
+    while(true){
+        fb = esp_camera_fb_get();
+        if (!fb) {
+            ESP_LOGE(TAG, "Camera capture failed");
+            res = ESP_FAIL;
+            break;
+        }
+        if(fb->format != PIXFORMAT_JPEG){
+            bool jpeg_converted = frame2jpg(fb, 80, &_jpg_buf, &_jpg_buf_len);
+            if(!jpeg_converted){
+                ESP_LOGE(TAG, "JPEG compression failed");
+                esp_camera_fb_return(fb);
+                res = ESP_FAIL;
+            }
+        } else {
+            _jpg_buf_len = fb->len;
+            _jpg_buf = fb->buf;
+        }
+
+        if(res == ESP_OK){
+            res = httpd_resp_send_chunk(req, _STREAM_BOUNDARY, strlen(_STREAM_BOUNDARY));
+        }
+        if(res == ESP_OK){
+            size_t hlen = snprintf((char *)part_buf, 64, _STREAM_PART, _jpg_buf_len);
+
+            res = httpd_resp_send_chunk(req, (const char *)part_buf, hlen);
+        }
+        if(res == ESP_OK){
+            res = httpd_resp_send_chunk(req, (const char *)_jpg_buf, _jpg_buf_len);
+        }
+        if(fb->format != PIXFORMAT_JPEG){
+            free(_jpg_buf);
+        }
+        esp_camera_fb_return(fb);
+        if(res != ESP_OK){
+            break;
+        }
+        int64_t fr_end = esp_timer_get_time();
+        int64_t frame_time = fr_end - last_frame;
+        last_frame = fr_end;
+        frame_time /= 2000;
+        ESP_LOGI(TAG, "MJPG: %uKB %ums (%.1ffps)",
+            (uint32_t)(_jpg_buf_len/1024),
+            (uint32_t)frame_time, 1000.0 / (uint32_t)frame_time);
+    }
+
+    last_frame = 0;
+    return res;
+}
+
+httpd_uri_t uri_get = {
+    .uri = "/",
+    .method = HTTP_GET,
+    .handler = jpg_stream_httpd_handler,
+    .user_ctx = NULL};
+httpd_handle_t setup_server(void)
+{
+    httpd_config_t config = HTTPD_DEFAULT_CONFIG();
+    httpd_handle_t stream_httpd  = NULL;
+
+    if (httpd_start(&stream_httpd , &config) == ESP_OK)
+    {
+        httpd_register_uri_handler(stream_httpd , &uri_get);
+    }
+
+    return stream_httpd;
 }
 
 void print_inference_result(ei_impulse_result_t result) {
@@ -92,40 +221,74 @@ void print_inference_result(ei_impulse_result_t result) {
 
 extern "C" int app_main()
 {
-    setup_led();
+    // Initialize NVS
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
+    {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+
+    connect_wifi();
+
+    if (!wifi_connect_status)
+    {
+        ei_printf("Failed to connect to WiFi\n");
+        return 1;
+    }
+    ei_printf("Connected to WiFi\n");
+
+    // Initialize Camera
+    int cam_status = app_camera_init();
+    if (cam_status != 0) {
+        ei_printf("Camera init failed\n");
+    }
     ei_sleep(100);
 
-    ei_impulse_result_t result = { nullptr };
+    // Start the webserver
+    setup_server();
+    ei_printf("Webserver started\n");
 
-    ei_printf("Edge Impulse standalone inferencing (Espressif ESP32)\n");
-
-    if (sizeof(features) / sizeof(float) != EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE)
-    {
-        ei_printf("The size of your 'features' array is not correct. Expected %d items, but had %u\n",
-                EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE, sizeof(features) / sizeof(float));
+    snapshot_buf = (uint8_t*)malloc(EI_CAMERA_RAW_FRAME_BUFFER_COLS * EI_CAMERA_RAW_FRAME_BUFFER_ROWS * EI_CAMERA_FRAME_BYTE_SIZE);
+    // check if allocation was successful
+    if (snapshot_buf == nullptr) {
+        ei_printf("ERR: Failed to allocate snapshot buffer!\n");
         return 1;
     }
 
+    ei_impulse_result_t result = { nullptr };
+    ei_printf("Edge Impulse standalone inferencing (Espressif ESP32)\n");
+
     while (true)
     {
-        // blink LED
-        gpio_set_level(LED_PIN, 1);
-
         // the features are stored into flash, and we don't want to load everything into RAM
-        signal_t features_signal;
-        features_signal.total_length = sizeof(features) / sizeof(features[0]);
-        features_signal.get_data = &raw_feature_get_data;
+        ei::signal_t signal;
+        signal.total_length = EI_CLASSIFIER_INPUT_WIDTH * EI_CLASSIFIER_INPUT_HEIGHT;
+        signal.get_data = &ei_camera_get_data;
+
+        /*  Makes the call to the ei_camera_capture function,
+            which captures the image to the framebuffer,
+            converts the image to the correct format and resizes it.
+        */
+        if (get_image((size_t)EI_CLASSIFIER_INPUT_WIDTH, (size_t)EI_CLASSIFIER_INPUT_HEIGHT, snapshot_buf) == false) {
+            ei_printf("Failed to capture image\r\n");
+            free(snapshot_buf);
+            return 1;
+        }
 
         // invoke the impulse
-        EI_IMPULSE_ERROR res = run_classifier(&features_signal, &result, false /* debug */);
-        if (res != EI_IMPULSE_OK) {
-            ei_printf("ERR: Failed to run classifier (%d)\n", res);
-            return res;
+        EI_IMPULSE_ERROR err = run_classifier(&signal, &result, false);
+        if (err != EI_IMPULSE_OK) {
+            ei_printf("ERR: Failed to run classifier (%d)\n", err);
+            return 1;
         }
+
+        // Print the predictions to Serial Debug
+        ei_printf("TFT Example -  Predictions (DSP: %d ms., Classification: %d ms., Anomaly: %d ms.): \n",
+            result.timing.dsp, result.timing.classification, result.timing.anomaly);
 
         print_inference_result(result);
 
-        gpio_set_level(LED_PIN, 0);
         ei_sleep(1000);
     }
 }
